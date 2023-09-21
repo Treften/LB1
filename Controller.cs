8b9163da6189bdfa758ff67d5066636ca34c2786
Treften <mamont.wild@gmail.com>
2023-09-21 17:27:45 +0700

    final??

---
+++ Controller.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LB1
+{
+   
+    class Controller
+    {
+        public static List<Pawn> list = new List<Pawn>();
+         static   string filePath = "file.csv";
+     public   static string PrintPawn(Pawn pawn)
+        {
+            string married = "";
+            if (pawn.married)
+            {
+                married = "Женат";
+            }
+            else
+            {
+                married = "Холост";
+            }
+          string s= "\n\n" + "Имя: " + pawn.Name + "\n"
+                + "Фамилия: " + pawn.Surname + "\n"
+                + "Адрес: " + pawn.Address + "\n"
+                + "Возраст: " + pawn.Age + "\n"
+                + married;
+            return s;
+        }
+        public static string PrintAll()
+        {
+            string s="";
+            foreach (Pawn pawn in list)
+            {
+             s+=   PrintPawn(pawn);
+            }
+            return s;
+        }
+        public static string PrintByIndex(int ind)
+        {
+            if (GetByIndex(list, ind) != null)
+            {
+               return PrintPawn(GetByIndex(list, ind));
+            }
+            else
+            {
+                return "Индекс не найден";
+            }
+        }
+        public static string DeleteByIndex(int ind)
+        {
+            if (GetByIndex(list, ind) != null)
+            {
+                list.RemoveAt(ind);
+                return ("\nДанные удалены");
+            }
+            else
+            {
+                return "Индекс не найден";
+            }
+        }
+        public static void SaveFile()
+        {
+            using (var writer = new StreamWriter(filePath))
+            {
+                foreach (Pawn pawn in list)
+                {
+                    writer.WriteLine(pawn.Name + "," + pawn.Surname + "," + pawn.Address + "," + pawn.Age + "," + pawn.married);
+                }
+
+            }
+        }
+            public static Pawn GetByIndex(List<Pawn> list, int index)
+        {
+            if (index < list.Count && index > 0)
+            {
+                return list[index];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public static void ReadFile()
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    var values = line.Split(',');
+                    if (line.Length < 5)
+                    {
+                        Console.WriteLine("Недостаточно данных");
+                        Environment.Exit(0);
+                    }
+                    list.Add(new Pawn(values[0], values[1], values[2], Convert.ToInt32(values[3]), Convert.ToBoolean(values[4])));
+
+                }
+            }
+        }
+    }
+}
--- View.cs
+++ View.cs
@@ -4,66 +4,12 @@ using System.IO;
 
 namespace LB1
 {
-    class Program
+    class View
     {
-        static void PrintPawn(Pawn pawn)
-        {
-            string married = "";
-            if (pawn.married)
-            {
-                married = "Женат";
-            }
-            else
-            {
-                married = "Холост";
-            }
-            Console.WriteLine("\n\n" + "Имя: " + pawn.Name + "\n"
-                + "Фамилия: " + pawn.Surname + "\n" 
-                + "Адрес: " + pawn.Address + "\n"
-                + "Возраст: " + pawn.Age + "\n"
-                + married);
-
-        }
-        static void PrintAll(List<Pawn> list)
-        {
-            foreach(Pawn pawn in list)
-            {
-                PrintPawn(pawn);
-            }
-
-        }
-        static Pawn GetByIndex(List<Pawn> list, int index)
-        {
-            if (index < list.Count&&index>0 )
-            {
-                return list[index];
-            }
-            else
-            {
-                return null;
-            }
-        }
+      
             static void Main(string[] args)
         {
-            List<Pawn> list = new List<Pawn>();
-            string filePath = "file.csv";
-
-            using (var reader = new StreamReader(filePath))
-            {
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-
-                    var values = line.Split(',');
-                    if (line.Length < 5)
-                    {
-                        Console.WriteLine("Недостаточно данных");
-                        Environment.Exit(0);
-                    }
-                    list.Add(new Pawn(values[0], values[1], values[2], Convert.ToInt32(values[3]), Convert.ToBoolean(values[4]) ) );
-               
-                }
-            }
+            Controller.ReadFile();
             Console.WriteLine("\n1 -- Вывести все записи\n2 -- Вывести по индексу\n3 -- Добавить запись\n4 -- Удалить запись\n5 -- Сохранить данные");
             System.ConsoleKey key = Console.ReadKey().Key;
             while (key != ConsoleKey.Escape)
@@ -72,28 +18,20 @@ namespace LB1
                 switch (key)
                     {
                     case ConsoleKey.D1:
-                        PrintAll(list);
+                    Console.WriteLine (Controller.PrintAll());
                     break;
                     case ConsoleKey.D2:
                         Console.Write("\nВведите индекс: ");
                         try
                         {
                             int ind = Convert.ToInt32(Console.ReadLine())+1;
-                            if(GetByIndex(list, ind)!=null)
-                            {
-                                PrintPawn(GetByIndex(list, ind));
-                            }
-                            else
-                            {
-                                Console.WriteLine("Индекс не найден");
-                            }
+                           Console.WriteLine (Controller.PrintByIndex(ind));
                             
                         }
                         catch (Exception e)
                         {
                             Console.WriteLine(e.Message);
                         }
-
                         break;
                     case ConsoleKey.D3:
                         try
@@ -124,7 +62,7 @@ namespace LB1
                                     married = false;
                                 }
                             }
-                            list.Add(new Pawn(name, surname, address, age, married));
+                            Controller.list.Add(new Pawn(name, surname, address, age, married));
                             Console.WriteLine("\nДанные добавлены");
                         }
                         catch (Exception e)
@@ -137,33 +75,18 @@ namespace LB1
                         try
                         {
                             int ind = Convert.ToInt32(Console.ReadLine())+1;
-                            if (GetByIndex(list, ind) != null)
-                            {
-                                list.RemoveAt(ind);
-                                Console.WriteLine("\nДанные удалены");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Индекс не найден");
-                            }
+                            Console.WriteLine(Controller.DeleteByIndex(ind));
 
                         }
                         catch (Exception e)
                         {
                             Console.WriteLine(e.Message);
                         }
-
+                        Console.WriteLine("\nДанные удалены");
                         break;
                     case ConsoleKey.D5:
-                        using (var writer = new StreamWriter(filePath))
-                        {
-                            foreach (Pawn pawn in list)
-                            {
-                                writer.WriteLine(pawn.Name+","+ pawn.Surname + "," + pawn.Address + "," + pawn.Age + "," + pawn.married  );
-                            }
-                                
-                        }
-                        Console.WriteLine("Данные сохранены");
+                       
+                        Console.WriteLine("\nДанные сохранены");
                             break;
                 }
                 Console.WriteLine("\n1 -- Вывести все записи\n2 -- Вывести по индексу\n3 -- Добавить запись\n4 -- Удалить запись\n5 -- Сохранить данные");
